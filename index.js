4cf9b9e5bdcfa5044815ba1f8ccf85b1368027c3
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2021-03-19 17:01:03 +0100

    suppression des anciennes affiches, ajout d'un lien et des descriptions sur les affiches

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ const postersProchainement = document.querySelector("#posters-alaffiche");
 const films = {
     alAffiche: [
         {
-            name: "film 1",
-            picture: "assets/affiche1.jpg",
-            description: "blablabla"
+            name: "PULL REQUEST FOR A DREAM",
+            picture: "assets/lefilm3.png",
+            description: "«Jared est un addicte à Git. Lors de ses projets, il ne peut s'empêcher de push  après avoir tapé 3 lignes de codes, il va tomber dans une spirale infernale de pull request»"
         },
         {
-            name: "film 2",
-            picture: "assets/affiche2.jpg",
-            description: "blablabla"
+            name: "PROJET 3",
+            picture: "assets/lefilm5.png",
+            description: "«Elle vient tout juste de rejoindre la Wild, tout se passait si bien, pourtant une idée lui hante déjà l'esprit. Et si le client était chiant ? Et si elle ne rendait jamais le projet ? Un thriller à vous donner des cauchemars.»" 
         },
         {
-            name: "film 3",
-            picture: "assets/affiche3.jpg",
-            description: "blablabla"
+            name: "THE CODE FATHER ",
+            picture: "assets/lefilm6.png",
+            description: "«Bordeaux rive droite, 2021. Une guerre de clan oppose depuis des années les plus grandes mafias de la région. Les PHP et les JavaScript. L'arrivée de nouveaux Wilders peut-elle tout changer ?»"
         }
     ],
     prochainement: [
         {
-            name: "film 4",
-            picture: "assets/ET.png",
-            description: "blablabla"
+            name: "BACK TO THE MAIN BRANCH",
+            picture: "assets/lefilm4.png",
+            description: "«Un groupe d'étudiant en plein projet technique se retrouve piégé par le temps dans une branche dev pleine de commits approximatifs. Vont ils rendre leurs projets sur la main branche à temps ?»"
         },
         {
-            name: "film 5",
-            picture: "assets/monopoly.jpg",
-            description: "blablabla"
+            name: "JAVASCRIPT PARK",
+            picture: "assets/lefilm2.png",
+            description: "«Quentin B, instructeur de la puissante compagnie nommée WildCodeSchool, parvient à inculquer les bases de JavaScript à de nouveaux Wilders et, étonné de leurs prouesses, décide d'ouvrir un parc d'attraction sur la place des Quinconces.»"
         },
         {
-            name: "film 6",
-            picture: "assets/titanic.jpg",
-            description: "blablabla"
+            name: "UN ELEPHANT SUR LES BRAS",
+            picture: "assets/lefilm1.png",
+            description: "«Charmés par le discours de leur instructeur, ce groupe de Wilder ont emprunté la voie du PHP. C'était sans réaliser dans quel aventure ils allaients embarquer. Une comédie touchante à découvrir en famille !»"
         },
     ]
 }
@@ -47,10 +47,14 @@ function createPoster(parent, title, imgUrl, description) {
     posterHeader.classList.add("poster-header");
     poster.appendChild(posterHeader);
 
+    const linkToReserve = document.createElement("a");
+    linkToReserve.href = "reserve.html";
+    posterHeader.appendChild(linkToReserve);
+
     const posterImg = document.createElement("img");
     posterImg.src = `${imgUrl}`;
     posterImg.classList.add("poster-img");
-    posterHeader.appendChild(posterImg);
+    linkToReserve.appendChild(posterImg);
 
     const posterBody = document.createElement("div");
     posterBody.classList.add("poster-body");
